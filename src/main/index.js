2e4af20ca0ebe04e7f6085e756c835df1432ad13
covidisahoax <OothecaPickle@users.noreply.github.com>
2024-12-03 16:02:32 -0800

    focus windows properly on macOS
    * showWindowNow has been false by default since commit af6d4abf3ab870758300b5d773beb25cd39e5a54, however, i can find no reason given for this being the case
    * as is, this causes windows created on macOS to not be properly focused
    
    * focus app after attempting to invoke a second instance of FreeTube, & steal focus from other apps on macOS

--- src/main/index.js
+++ src/main/index.js
@@ -291,6 +291,7 @@ function runApp() {
             // Just focus the main window (instead of starting a new instance)
             if (mainWindow.isMinimized()) mainWindow.restore()
             mainWindow.focus()
+            app.focus({ steal: true })
 
             if (url) mainWindow.webContents.send(IpcChannels.OPEN_URL, url)
           }
@@ -683,7 +684,7 @@ function runApp() {
     {
       replaceMainWindow = true,
       windowStartupUrl = null,
-      showWindowNow = false,
+      showWindowNow = true,
       searchQueryText = null
     } = { }) {
     // Syncing new window background to theme choice.
