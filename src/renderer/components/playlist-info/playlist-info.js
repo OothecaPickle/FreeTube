7586abd8cb9ce6daa0fd63f0570b37ec5dfa0b28
absidue <48293849+absidue@users.noreply.github.com>
2025-01-12 01:44:10 +0100

    Replace lodash.debounce with a much smaller, custom debounce function (#6550)
    * Replace lodash.debounce with a much smaller, custom debounce function
    
    * Fix grammar in code comment

--- src/renderer/components/playlist-info/playlist-info.js
+++ src/renderer/components/playlist-info/playlist-info.js
@@ -8,13 +8,13 @@ import FtPrompt from '../ft-prompt/ft-prompt.vue'
 import FtButton from '../ft-button/ft-button.vue'
 import {
   ctrlFHandler,
+  debounce,
   formatNumber,
   showToast,
   getTodayDateStrLocalTimezone,
   writeFileFromDialog,
   showSaveDialog,
 } from '../../helpers/utils'
-import debounce from 'lodash.debounce'
 import thumbnailPlaceholder from '../../assets/img/thumbnail_placeholder.svg'
 
 export default defineComponent({
