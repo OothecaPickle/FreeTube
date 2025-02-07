7a5f528adb4bb7c8780e1aef83407f6e91e9825b
absidue <48293849+absidue@users.noreply.github.com>
2025-02-07 03:05:40 +0100

    Request the single instance lock as soon as possible (#6752)

--- src/datastores/handlers/base.js
+++ src/datastores/handlers/base.js
@@ -336,6 +336,17 @@ class SubscriptionCache {
   }
 }
 
+function loadDatastores() {
+  return Promise.allSettled([
+    db.settings.loadDatabaseAsync(),
+    db.history.loadDatabaseAsync(),
+    db.profiles.loadDatabaseAsync(),
+    db.playlists.loadDatabaseAsync(),
+    db.searchHistory.loadDatabaseAsync(),
+    db.subscriptionCache.loadDatabaseAsync(),
+  ])
+}
+
 function compactAllDatastores() {
   return Promise.allSettled([
     db.settings.compactDatafileAsync(),
@@ -355,5 +366,6 @@ export {
   SearchHistory as searchHistory,
   SubscriptionCache as subscriptionCache,
 
+  loadDatastores,
   compactAllDatastores,
 }
